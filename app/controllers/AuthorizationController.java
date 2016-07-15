39b07137c72144043380a139da290b93c008156f
taviss <tavi7395@yahoo.com>
2016-07-15 17:02:39 +0300

    Initial authorization system commit

---
+++ app/controllers/AuthorizationController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import play.mvc.Result;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import static play.mvc.Results.ok;
+
+/**
+ * Created by octavian.salcianu on 7/15/2016.
+ */
+public class AuthorizationController {
+
+    public Result tryLogin() {
+        return ok();
+    }
+    public static byte[] hashPassword( final char[] password, final byte[] salt, final int iterations, final int keyLength ) {
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance( "PBKDF2WithHmacSHA512" );
+            PBEKeySpec spec = new PBEKeySpec( password, salt, iterations, keyLength );
+            SecretKey key = skf.generateSecret( spec );
+            byte[] res = key.getEncoded( );
+            return res;
+
+        } catch( NoSuchAlgorithmException | InvalidKeySpecException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+}
