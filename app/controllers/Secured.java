a49e159ee1c7052b33eb73b3d121a3582531686f
taviss <tavi7395@yahoo.com>
2016-07-18 15:02:38 +0300

    Added Secured class to control access

---
+++ app/controllers/Secured.java
@@ -0,0 +1,30 @@
+package controllers;
+
+/**
+ * Created by octavian.salcianu on 7/18/2016.
+ */
+
+import models.dao.UserDAO;
+import play.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import models.*;
+
+public class Secured extends Security.Authenticator {
+
+    @Override
+    public String getUsername(Context ctx) {
+        return ctx.session().get("user");
+    }
+
+    @Override
+    public Result onUnauthorized(Context ctx) {
+        return redirect(routes.Application.index());
+    }
+
+    public static int getAdminLevel() {
+        UserDAO ud = new UserDAO();
+        User u = ud.getUserName(Context.current().request().username());
+        return u.getAdminLevel();
+    }
+}
