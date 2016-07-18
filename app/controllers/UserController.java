9acd0ba610b999cacbcf330542c809c8d6cd4ac4
taviss <tavi7395@yahoo.com>
2016-07-18 17:11:35 +0300

    Added basic search endpoint logic + ProductDAO operation

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,17 +1,36 @@
 package controllers;
 
+import models.Product;
 import models.User;
+import models.dao.ProductDAO;
 import models.dao.UserDAO;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.db.jpa.Transactional;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import views.html.index;
 import views.html.user;
+
+import java.util.List;
 
 /**
  * Created by octavian.salcianu on 7/11/2016.
  */
 public class UserController extends Controller {
+
+    @Security.Authenticated(Secured.class)
+    @Transactional
+    public Result trySearch(String productName) {
+        //DynamicForm requestData = Form.form().bindFromRequest();
+        //String productName = requestData.get("productName");
+        ProductDAO pd = new ProductDAO();
+        List<Product> products = pd.findProductsByName(productName);
+        if(products.isEmpty()) return notFound();
+        else return ok(Json.toJson(products));
+    }
     /**
      * Start indexing w/ threads?
      */
--- app/models/dao/ProductDAO.java
+++ app/models/dao/ProductDAO.java
@@ -1,16 +1,39 @@
 package models.dao;
 
+import models.Product;
 import play.db.jpa.JPA;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 /**
  * Created by octavian.salcianu on 7/14/2016.
  */
 public class ProductDAO {
-    /**
-     * TBA
-     */
+    private EntityManager em;
+    private CriteriaBuilder criteriaBuilder;
+
+    public ProductDAO() {
+        this.em = JPA.em();
+        this.criteriaBuilder = em.getCriteriaBuilder();
+    }
+
+    public List<Product> findProductsByName(String productName) {
+        CriteriaQuery<Product> criteriaQuery = this.criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+        Predicate prodNameP = this.criteriaBuilder.like(root.get("prodName"), productName);
+
+        criteriaQuery.where(prodNameP);
+        Query query = this.em.createQuery(criteriaQuery);
+        @SuppressWarnings("unchecked")
+        List<Product> foundProducts = (List<Product>) query.getResultList();
+        return foundProducts;//empty check in controller
+    }
 }
