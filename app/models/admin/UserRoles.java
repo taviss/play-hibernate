ef4f4ecc44a3eeab6072f6dadb57e61c51fe7fe4
taviss <tavi7395@yahoo.com>
2016-07-22 11:33:00 +0300

    Added user roles

---
+++ app/models/admin/UserRoles.java
@@ -0,0 +1,11 @@
+package models.admin;
+
+/**
+ * Created by octavian.salcianu on 7/22/2016.
+ */
+public class UserRoles {
+    public static final int NORMAL_USER = 0;
+    public static final int ASSISTANT = 1;
+    public static final int MODERATOR = 2;
+    public static final int LEAD_ADMIN = 3;
+}
