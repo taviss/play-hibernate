723e590a6c4aeb5ddd20a376acb7b0ca55185b1b
taviss <tavi7395@yahoo.com>
2016-08-11 14:58:54 +0300

    Added search history for any user for administrators + modified some routes

--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -4,9 +4,11 @@ import com.google.inject.Inject;
 import models.Product;
 import models.SearchHistory;
 import models.User;
+import models.admin.UserRoles;
 import models.dao.ProductDAO;
 import models.dao.SearchHistoryDAO;
 import models.dao.UserDAO;
+import play.Logger;
 import play.db.jpa.Transactional;
 import play.libs.Json;
 import play.mvc.Http;
@@ -14,9 +16,11 @@ import play.mvc.Result;
 import play.mvc.Security;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import static play.mvc.Controller.request;
+import static play.mvc.Results.badRequest;
 import static play.mvc.Results.notFound;
 import static play.mvc.Results.ok;
 
@@ -40,14 +44,54 @@ public class SearchController {
         //String productName = requestData.get("productName");
         Set<Map.Entry<String,String[]>> queryString = request().queryString().entrySet();
         Set<Product> products = productDAO.findProductsByName(productName, queryString);
-        SearchHistory searchHistory = new SearchHistory();
-        searchHistory.setQueryString(productName);
-        searchHistory.setInputDate(new Date());
-        User user = userDAO.getUserByName(Http.Context.current().request().username());
-        searchHistory.setUser(user);
-        searchHistoryDAO.create(searchHistory);
+
+        try {
+            SearchHistory searchHistory = new SearchHistory();
+            searchHistory.setQueryString(productName);
+            searchHistory.setInputDate(new Date());
+            User user = userDAO.getUserByName(Http.Context.current().request().username());
+            searchHistory.setUser(user);
+            searchHistoryDAO.create(searchHistory);
+        } catch (NullPointerException e) {
+            Logger.error("Error while inserting search history(" + request().username() + "[" + request().remoteAddress() + "]) " + e.getMessage());
+        }
 
         if (products.isEmpty()) return notFound();
         else return ok(Json.toJson(products));
+    }
+
+    @Security.Authenticated(Secured.class)
+    @Transactional
+    public Result searchHistory() {
+        List<SearchHistory> searchHistory = null;
+        try {
+            User user = userDAO.getUserByName(Http.Context.current().request().username());
+            searchHistory = searchHistoryDAO.getUserSearchHistory(user.getId());
+        } catch (NullPointerException e) {
+            Logger.error("Error while fetching search history(" + request().username() + "[" + request().remoteAddress() + "]) " + e.getMessage());
+        }
+
+        if(searchHistory != null) {
+            if (searchHistory.isEmpty()) return notFound("No history yet");
+            else return ok(Json.toJson(searchHistory));
+        } else return badRequest("Session error");
+    }
+
+    @Security.Authenticated(Secured.class)
+    @Transactional
+    public Result getUserSearchHistory(Long id) {
+        if (Secured.getAdminLevel() != UserRoles.LEAD_ADMIN) {
+            return badRequest("Not enough privileges");
+        }
+
+        User user = userDAO.get(id);
+        try {
+            List<SearchHistory> searchHistory = searchHistoryDAO.getUserSearchHistory(user.getId());
+
+            if (searchHistory.isEmpty()) return notFound("No history yet");
+            else return ok(Json.toJson(searchHistory));
+        } catch (NullPointerException e) {
+            return notFound("No such user");
+        }
     }
 }
